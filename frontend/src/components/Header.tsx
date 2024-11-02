8a8c26707e9ed931df0d1e5710239e160f82e523
JuanFr1 <72809497+JuanFr1@users.noreply.github.com>
2024-11-02 02:28:20 -0500

    Correcciones Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,29 +12,33 @@ const Header = () => {
 
   return (
     <header className="header">
-      <img src={papilon} alt="Your Logo" className="logo" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-        <div className="searchbar">
-        <input type="text"
-        name="search"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={handleSearch}/>
-        <span className="searchbar-icon"><i className="fas fa-search"></i></span>
+      <div className="logo-container">
+        <img src={papilon} alt="Your Logo" className="logo" />
+      </div>
+      <div className="content-container">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+          <div className="searchbar">
+          <input type="text"
+          name="search"
+          placeholder="Buscar..."
+          value={searchQuery}
+          onChange={handleSearch}/>
+          <span className="searchbar-icon"><i className="fas fa-search"></i></span>
+          </div>
+        
+        <div className="language-selector">
+          <select className='selector'>
+            <option value="es">Español (América Latina)</option>
+            <option value="en">English</option>
+            {/* Add more language options as needed */}
+          </select>
         </div>
-
+        <div className="auth-buttons">
+          <button className="login-button">Iniciar Sesión</button>
+          <button className="register-button">Registro</button>
+        </div>
+      </div>
       
-      <div className="language-selector">
-        <select>
-          <option value="es">Español (América Latina)</option>
-          <option value="en">English</option>
-          {/* Add more language options as needed */}
-        </select>
-      </div>
-      <div className="auth-buttons">
-        <button className="login-button">Log In</button>
-        <button className="register-button">Register</button>
-      </div>
     </header>
   );
 };
