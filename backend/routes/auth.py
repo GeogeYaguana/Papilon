0857a304ac63e90fb7cd5895d62edd950b761454
georgeyaguana <sebyaguana@gmail.com>
2024-12-13 16:24:39 -0500

    completar Crud cliente locales , anadir seccion de idcliente

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -4,6 +4,8 @@ from extensions import bcrypt, jwt, get_session
 from flask_jwt_extended import create_access_token
 from sqlalchemy.exc import SQLAlchemyError
 
+from routes import cliente
+
 auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/login', methods=['POST'])
@@ -15,11 +17,22 @@ def login():
     with get_session() as session:
         usuario = session.query(Usuario).filter_by(usuario_nombre=usuario_nombre).first()
         if usuario and bcrypt.check_password_hash(usuario.password, password):
-            access_token = create_access_token(identity={'usuario_nombre': usuario.usuario_nombre})
-            return jsonify({'message': 'Inicio de sesión exitoso',
-                             'token': access_token ,
-                             'id_usuario' : usuario.id_usuario,
-                             'tipo_usuario': usuario.tipo_usuario
-                               }), 200
+            # Cambiar la identidad a id_usuario
+            access_token = create_access_token(identity=usuario.id_usuario)
+            return jsonify({
+                'message': 'Inicio de sesión exitoso',
+                'token': access_token,
+                'id_usuario': usuario.id_usuario,
+                'tipo_usuario': usuario.tipo_usuario
+            }), 200
         else:
             return jsonify({'error': 'Usuario o contraseña incorrectos'}), 401
+@auth_bp.route('/get_cliente', methods=['GET'])
+def get_cliente():
+    id_usuario = request.args.get('id_usuario', type=int)
+    with get_session() as session:
+        cliente = session.query(Cliente).filter_by(id_usuario=id_usuario).first()
+        if cliente:
+            return jsonify({'id_cliente': cliente.id_cliente}), 200
+        else:
+            return jsonify({'error': 'Cliente no encontrado'}), 404
